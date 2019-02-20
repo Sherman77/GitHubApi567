025f3488bf306cdbe3940b1025192394041fffdd
Sherman <sherman77.xu@gmail.com>
2019-02-19 20:54:42 -0500

    Update SSW567_HW4_Xiaomeng_Xu.py

--- SSW567_HW4_Xiaomeng_Xu.py
+++ SSW567_HW4_Xiaomeng_Xu.py
@@ -14,7 +14,7 @@ def get_id():
 def get_repository(id):
     """Based on user's ID, retrive names of his/her repositories, return as a list"""
     url = f"https://api.github.com/users/{id}/repos"
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'}
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'}    #Add header to shove HTTP err 403
     req = urllib.request.Request(url = url, headers = headers)
     uh = urllib.request.urlopen(req)   #Request data through URL
     data = uh.read().decode()  
@@ -27,7 +27,7 @@ def get_repository(id):
 def get_commit(id, repo_name):
     """Get number of commits based on name of repository, count the commits and return as a number"""
     url = f"https://api.github.com/repos/{id}/{repo_name}/commits"
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'}
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'}    #Add header to shove HTTP err 403
     req = urllib.request.Request(url = url, headers = headers)
     uh = urllib.request.urlopen(req)
     data = uh.read().decode()
